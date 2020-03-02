09b9eba861e5d3f9a472f8e2d1d47348fd877176
Rennan Rocha Ribas <rennanrr@hotmail.com>
2020-03-02 17:49:58 -0300

    basic table is on the table

--- src/components/DroneList.vue
+++ src/components/DroneList.vue
@@ -15,17 +15,33 @@
     </div>
     <div class="col-md-6">
       <h4>Drone List</h4>
-      <ul class="list-group">
-        <li class="list-group-item"
-          :class="{ active: index == currentIndex }"
-          v-for="(drone, index) in drones"
-          :key="index"
-          @click="setActiveDrone(drone, index)"
-        >
-          {{ drone.name }}
-        </li>
-      </ul>
-
+      <b-table-simple hover small caption-top responsive>
+        <b-thead head-variant="dark">
+          <b-tr>
+            <b-th>DRONE</b-th>
+            <b-th>CUSTOMER</b-th>
+            <b-th>BATTERIES</b-th>
+            <b-th>MAX SPEED</b-th>
+            <b-th>AVERAGE SPEED</b-th>
+            <b-th>CURRENT FLY</b-th>
+            <b-th>STATUS</b-th>
+          </b-tr>
+        </b-thead>
+        <b-tbody>
+          <b-tr v-for="(drone, index) in drones" :key="index" @click="setActiveDrone(drone, index)">
+            <b-th>{{ drone.id }}</b-th>
+            <b-td>
+              <h5> {{ drone.name }} </h5>
+              <small>{{ drone.address }}</small>
+            </b-td>
+            <b-td>{{ drone.battery }}</b-td>
+            <b-td>{{ drone.max_speed }}</b-td>
+            <b-td>{{ drone.average_speed }}</b-td>
+            <b-td>{{ drone.fly }}</b-td>
+            <b-td>{{ drone.status }}</b-td>
+          </b-tr>
+        </b-tbody>
+      </b-table-simple>
     </div>
     <div class="col-md-6">
       <div v-if="currentDrone">
